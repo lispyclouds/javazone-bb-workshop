9f7175c3d0bd71273eeab2b36464bf76a6ed8941
Rahul De <rahul080327@gmail.com>
2023-09-05 14:47:24 +0200

    [event] add clean fns

--- src/lambda/main.clj
+++ src/lambda/main.clj
@@ -1,6 +1,19 @@
 (ns lambda.main
   (:require
+   [cheshire.core :as json]
+   [clojure.string :as str]
    [lambda.runtime :as runtime]))
+
+(def pii-patterns
+  [#"\S+@\S+"])
+
+(defn clean
+  [s pattern]
+  (str/replace s pattern "<REDACTED>"))
+
+(defn clean-all
+  [s]
+  (reduce clean s pii-patterns))
 
 (defn handler
   [{:keys [n1 n2]} _context]
@@ -9,3 +22,9 @@
 (defn -main
   [& _]
   (runtime/init handler))
+
+(comment
+  (clean "this email: foo@bar.com" (get pii-patterns 0))
+
+  (handler (json/parse-string (slurp "resources/event.json") true)
+           {}))
